5994acf1d87f10207965f2ca6317f0310928c924
Lian <lianmafutra@gmail.com>
2019-08-27 20:30:23 +0700

    add detail movie

---
+++ app/src/main/java/com/sunflower/submission1androidexpert/DetailMovie.java
@@ -0,0 +1,14 @@
+package com.sunflower.submission1androidexpert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
